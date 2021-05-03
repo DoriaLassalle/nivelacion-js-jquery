de1b691114f8faeff319599c23f3fa0d46314c4d
Doria <dorialassalle@yahoo.com>
2021-05-02 20:39:33 -0400

    almost ready, need to fix etapa 4

--- js/script-index.js
+++ js/script-index.js
@@ -1,7 +1,13 @@
 $(document).ready( function(){
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
+	
+	$(".js-back").hide();
+
+	printNews()
 	renderHighlightedRecipes(recipesArray);
+	renderActivities(activities);
+	
 
 });
 
@@ -11,6 +17,13 @@ $(document).ready( function(){
 * marcado el atributo "highlighted" como TRUE
 */
 function renderHighlightedRecipes(recipesArray) {
+
+	for (let i=0; i<recipesArray.length; i++){
+		if (recipesArray[i].highlighted==true){
+			console.log(recipesArray[i]);
+			renderRecipe(recipesArray[i]);
+		}
+	}
 	console.log('Recipes: ', recipesArray);
 }
 
@@ -20,9 +33,59 @@ function renderHighlightedRecipes(recipesArray) {
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-recipe.html"
 */
+let directions=[];
+let ingredients=[];
 function renderRecipe(recipe) {
+
+	$("#list").html(`<div>
+						<a class="item-recipe" href="recipe.html">
+							<span class="attribution">
+								<span class="title-recipe"> </span>
+								<span class="metadata-recipe">
+									<span class="author-recipe"> </span>
+									<span class="bookmarks-recipe">
+										<span class="icon-bookmark"></span> 
+									</span>
+								</span>
+							</span>							
+							<img src="URL DE LA IMAGEN" class="recipeImg"/>
+						</a>
+					</div>`);
+  
+	$(".title-recipe").text(recipe.title);
+	$(".author-recipe, .source").text(recipe.source.name);
+	$(".recipeImg").attr("src", recipe.source.url);
+	$(".description-recipe").text(recipe.excerpt);
+	$(".time").text(recipe.cooktime);
+	$(".list-ingredients").text(recipe.ingredients);//funct below
+	$(".list-directions").text(recipe.directions);//funct below
+	$("#otrolink").attr("href", recipe.source.url).text(recipe.source.name);
+
+	directions=recipe.directions;
+	ingredients=recipe.ingredients;
+
+/* 	addDirections(directions);
+	addIngredientes(ingredients); */
+
+	
 	console.log('Voy a pintar la receta: ', recipe);
 }
+
+/* function addDirections(directions){ //2 elementos
+	//console.log("lo que llega "+ directions.length);
+	for (let i=0; i<directions.length; i++){
+		$(".list-directions").html("<li class='item-direction'></li>")
+		$(".item-direction").text(directions[i]);
+	}
+}
+function addIngredientes(ingredients){//18 elementos
+	//console.log("lo que llega2 "+ ingredients.length);
+	for (let i=0; i<ingredients.length; i++){
+		$(".list-ingredients").html("<li class='ingred'></li>")
+		$(".ingred").text(ingredients[i]);
+	}
+
+} */
 
 
 
@@ -30,6 +93,14 @@ function renderRecipe(recipe) {
 * Función que se encarga de pintar todas las actividades
 */
 function renderActivities(activitiesArray) {
+	if (activitiesArray.length>0){
+		$(".wrapper-message").hide();
+	}
+
+	for (let i=0; i<activitiesArray.length; i++){
+		renderActivity(activitiesArray[i]);
+	}
+
 	console.log('Activities: ', activitiesArray);
 }
 
@@ -39,7 +110,34 @@ function renderActivities(activitiesArray) {
 * archivo "templates/templates-activity.html"
 */
 function renderActivity(recipe) {
-	
+
+	$("#actList").html(`<div>
+							<a href="#" class="item-activity">
+							<span class="attribution">								
+								<span class="avatar">
+									<img src="" class="image-avatar">
+								</span>										
+								<span class="meta">
+									<span class="author"></span> made 
+									<span id="actRecipe" class="recipe"></span>
+									<span class="location"></span>
+								</span>								
+							</span>
+							<div class="bg-image activity" style="background-image: url('IMAGEN DE RECETA (ATTRIBUTO "image")');"></div>
+							</a>
+						</div>`);
+
+	$(".image-avatar").attr("src", recipe.userAvatar);
+	$(".author").text(recipe.userName);
+	$("#actRecipe").text(recipe.recipeName+": "+recipe.text+". ");
+	$(".location").text(recipe.place);
+	$(".activity").css("background-image", "url('"+recipe.image+"')");
+
+
+}
+
+function printNews(){
+	$(".callout-news p").text("NUEVAS RECETAS");
 }
 
 
---
+++ js/script-recipe.js
@@ -0,0 +1,32 @@
+$(document).ready( function(){
+
+    console.log("Este documento está listo")
+ 
+    $(".js-menu").hide();
+    $(".js-back").show();
+ 
+    $("#btnRecipe").on("click", function(){
+      
+        $("#mainDiv").removeClass("make");
+        $("#btnRecipe").addClass("active");
+        $("#btnMake").removeClass("active");
+       
+    });
+
+    $("#btnMake").on("click", function(){
+        $("#mainDiv").addClass("make");
+        $("#btnMake").addClass("active");
+        $("#btnRecipe").removeClass("active");
+      
+
+    }); 
+
+    $(".icon-arrow-left-alt").on("click", function(){
+        console.log("hola raton con cola")
+        //ver que funcion le hago
+       
+    })
+
+
+});
+
